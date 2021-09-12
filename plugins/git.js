a24b08d9b0162d63fd47bdadde830a3a94c692bd
imashimaah <89404057+imashimaah@users.noreply.github.com>
2021-09-13 02:40:54 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "AMOLED-Mobile-Full-HD-Wallpaper-1080X1920-9.jpg";
+  r_text[0] = "https://i.imgur.com/yCYYyEJ.jpeg";
     
     
     var i = Math.floor(1*Math.random())
@@ -19,13 +19,13 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
-*owner number wa.me/941323434*
+*owner number wa.me/919895828468*
 
-*bot setting video : .*
+*bot setting video : https://youtu.be/RlSG4ZgPls0*
 
-*githublink : https://github.com/imashimaah/Amalser_v2*
+*githublink : https://github.com/cyberchekuthan/Amalser_v2*
 
-*audio commads : https://github.com/imashimaah/Amalser_v2*
+*audio commads : https://github.com/cyberchekuthan/Amalser_v2/tree/master/uploads*
 `}) 
 
 }));
